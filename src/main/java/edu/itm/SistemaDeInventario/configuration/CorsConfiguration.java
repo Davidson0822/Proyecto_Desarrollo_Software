bd80eab892e16421a3e24d4df08ae2b810581790
juzerx <juamca2007@gmail.com>
2026-05-20 17:09:08 -0500

    Correcion del front y seguridad cors

---
+++ src/main/java/edu/itm/SistemaDeInventario/configuration/CorsConfiguration.java
@@ -0,0 +1,22 @@
+package edu.itm.SistemaDeInventario.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:8081",
+                        "http://localhost:8082"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
